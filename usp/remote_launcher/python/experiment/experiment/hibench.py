80e3723ebd3c3399ed8cad03bfcf77e363b43ec2
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2015-11-12 16:26:49 +0900

    Add HiBench functionalities

--- usp/remote_launcher/python/experiment/experiment/hibench.py
+++ usp/remote_launcher/python/experiment/experiment/hibench.py
@@ -1,5 +1,6 @@
 from expyrimenter.core import SSH, Executor
 
+
 class HiBench:
     LOGGER = 'hibench'
 
@@ -9,6 +10,7 @@ class HiBench:
         if executor is None:
             executor = Executor()
         self.executor = executor
+        self._config = {}
 
     def clean_logs(self):
         cmd = 'rm -rf {}/report'.format(self._home)
@@ -16,15 +18,37 @@ class HiBench:
 
     def set_parallelism(self, cores, benchmark):
         """AKA reduce number. Benchmark example: 'sort'."""
-        config = '{}/workloads/{}/conf/10-{}-userdefine.conf'.format(self._home,
-            benchmark, benchmark)
+        config = '{}/workloads/{}/conf/10-{}-userdefine.conf'.format(
+            self._home, benchmark, benchmark)
         cmd = 'echo hibench.default.map.parallelism {:d} >{}'.format(cores,
                                                                      config)
-        shuffle = int(cores/2)
+        shuffle = int(cores / 2)
         cmd += '; echo hibench.default.shuffle.parallelism {:d} >>{}'.format(
             shuffle, config)
         title = 'Set parallelism'
         self._ssh(cmd, title, stdout=True)
+
+    def prepare(self, benchmark):
+        cmd = '{}/workloads/{}/prepare/prepare.sh'.format(self._home,
+                                                          benchmark)
+        self._ssh(cmd, 'prepare ' + benchmark)
+
+    def run(self, benchmark, system='spark', language='scala'):
+        cmd = '{}/workloads/{}/{}/{}/bin/run.sh'.format(self._home, benchmark,
+                                                        system, language)
+        self._ssh(cmd, 'running {} {} {}'.format(language, system, benchmark))
+
+    def add_config(self, key, value):
+        self._config[key] = value
+
+    def write_config(self, benchmark):
+        file = '{}/workloads/{}/conf/10-{}-userdefine.conf'.format(
+            self._home, benchmark, benchmark)
+        self._ssh('>' + file, 'empty {} config file'.format(benchmark))
+        self.executor.wait()
+        for key, value in self._config.items():
+            cmd = 'echo {} {} >>{}'.format(key, value, file)
+            self._ssh(cmd, 'config file: {} {}'.format(key, value), stdout=True)
 
     def _ssh(self, cmd, title, *args, **kwargs):
         ssh = SSH(self._host, cmd, title=title, logger_name=HiBench.LOGGER,
